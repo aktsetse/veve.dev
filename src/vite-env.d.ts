e2542a56fdf9bd1d98fca5309ade319f1ddccb4a
Amenuveve Tsetse <amenu@Amenuveves-MacBook-Pro.local>
2026-03-10 01:08:55 -0500

    new gsap edits to projects

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module "gsap-trial/SplitText" {
-  export { SplitText } from "gsap/SplitText";
-}
-
-declare module "gsap-trial/ScrollSmoother" {
-  export { ScrollSmoother } from "gsap/ScrollSmoother";
-}
